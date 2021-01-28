5f357f4e7d389e3c670584f42b24f52ffd14970e
Giuseppe <giusepperaffa1988@gmail.com>
2021-01-28 16:01:59 +0100

    prima parte js ok

---
+++ js/script.js
@@ -0,0 +1,38 @@
+var app = new Vue({
+  el : '#app',
+  data : {
+    newTodo: "",
+    todos: [
+      "Fare i compiti",
+      "Fare la spesa",
+      "Studiare",
+      "Ginnastica"
+    ],
+    todosDelete: []
+  },
+  methods: {
+    addTodo(){
+      if (this.newTodo.length < 4){
+        alert("la lunghezza deve essere maggiore di 3 caratteri");
+      } else {
+        this.todos.push(this.newTodo);
+        this.newTodo = "";
+        }
+      },
+
+    // ora dobbiamo passare l indice dell elemento che vogliamo eliminare :
+    deleteTodo(index){
+    // lo pushamo nella lista elementi eliminati
+    this.todosDelete.push(this.todos[index]);
+      //e lo togliamo dall'array con splice e indichiamo (1) che dobbiamo eliminare quello soltanto (possiamo indicare volendo dopo il numero come o con cosa sostituirlo, noi mettiamo una stringa )
+    this.todos.splice(index, 1);
+    alert("hai eliminato " + this.todos[index]);
+    },
+    restoreTodo(index){
+      this.todos.push(this.todosDelete[index]);
+      this.todosDelete.splice(index,1);
+    }
+
+
+  }
+});
