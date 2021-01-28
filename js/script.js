e105059a2eb478916ccde80c04079cae07a934dd
Giuseppe <giusepperaffa1988@gmail.com>
2021-01-28 22:24:53 +0100

    restore

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
+// Realizzare una todolist con Vue. La lista deve essere già popolata con alcuni elementi e si deve dare la possibilità di aggiungere nuovi item e di cancellarli.
+// Bonus 1): permettere ai file cancellati di finire in un'area 'cestino' dove possono essere eliminati del tutto oppure ripristinati.
+// Bonus 2): Dare la possibilità di eliminare tutti i file dal cestino
+// Aggiungete quello che più vi piace a piacere!
+// inserire il nuovo todo
+// modificare un todo Bonus
+// spostare il todo nel cestino
+// pulsante per spostare tutti i todo nel cestino Bonus
+// ripristinare dal cestino un todo  Bonus
+// eliminare definitivamente un todo dal cestino Bonus
+// pulsante per eliminare tutti i todo del cestino Bonus
+
 var app = new Vue({
+
   el : '#app',
   data : {
     newTodo: "",
@@ -27,13 +40,10 @@ var app = new Vue({
       //e lo togliamo dall'array con splice e indichiamo (1) che dobbiamo eliminare quello soltanto (possiamo indicare volendo dopo il numero come o con cosa sostituirlo, noi mettiamo una stringa )
     alert("hai eliminato " + this.todos[index]);
     this.todos.splice(index, 1);
-
     },
     restoreTodo(index){
       this.todos.push(this.todosDelete[index]);
       this.todosDelete.splice(index,1);
     }
-
-
   }
 });
